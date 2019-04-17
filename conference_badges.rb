6b75719f963bf41eb0642da4571629a770c748b5
Solomon Feldman <solomonrfeldman@gmail.com>
2019-04-17 16:23:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,6 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(names)
-  batch = []
-  names.each { |name| badge_maker(name) }
+  # batch = []
+  names.collect { |name| badge_maker(name) }
 end
